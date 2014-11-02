d1b5785f5bcd366aeb5112c969aeff343ec5a5d2
Merichbier <alain.birchmeier@outlook.com>
2014-11-02 21:40:41 +0100

    Find Digits
    HackerRank - Algorithms

---
+++ Find Digits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    //static void Main(String[] args)
+    //{
+    //    Int64 nbTest = Int64.Parse(Console.ReadLine());
+    //    for (int test = 0; test < nbTest; ++test)
+    //    {
+    //        Int64 N = Int64.Parse(Console.ReadLine());
+    //        solve(N);
+    //    }
+    //    Console.ReadLine();
+    //}
+
+    static void solve(Int64 number)
+    {
+        String numberStr = number.ToString();
+        Int64 count = 0;
+        for (int index = 0; index < numberStr.Length; ++index)
+        {
+            int divider = Int16.Parse(numberStr.Substring(index, 1));
+            if (divider != 0 && number % divider == 0) ++count;
+        }
+        Console.WriteLine(count);
+    }
+}
